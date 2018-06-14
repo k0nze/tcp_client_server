4d6506ffaf3304e75c84642ece387c7d38deba69
Konstantin Luebeck <konstantin.luebeck@uni-tuebingen.de>
2018-06-14 10:09:13 +0200

    NERDTree will be opened in the vim window.

--- ide.sh
+++ ide.sh
@@ -3,7 +3,7 @@
 SESSION_NAME=ide
 
 if [ $(tmux ls 2>&1 | grep ${SESSION_NAME} | wc -l) -eq 0 ]; then
-    tmux -2 new -s ${SESSION_NAME}\; send-keys 'vim' C-m\; split-window -v -p 20\; send-keys 'cd build' C-m\; select-pane -t 0
+    tmux -2 new -s ${SESSION_NAME}\; send-keys 'vim' C-m\; send-keys ':NERDTree' C-m\; split-window -v -p 20\; send-keys 'cd build' C-m\; select-pane -t 0
     else
     tmux a -t ${SESSION_NAME}
 fi
